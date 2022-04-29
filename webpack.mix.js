5191a5b2f3456a76816dddcd3a9f903273958f91
Thalisson Rodrigues <thalissonsv22@gmail.com>
2022-04-29 02:21:31 -0300

    configura eslint para arquivos .js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+require('laravel-mix-eslint');
 
 /*
  |--------------------------------------------------------------------------
@@ -14,3 +15,5 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sourceMaps();
+
+mix.eslint();
